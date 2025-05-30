6daff858dac175a5d820aa0f6c61d2d75cb10cc1
a-sandoval <asandoval@frba.utn.edu.ar>
2025-05-30 20:54:21 -0300

    cambio detalle de Productos

--- src/mockData/products.js
+++ src/mockData/products.js
@@ -4,5 +4,5 @@ export const products  = [
   { id: 3, image: process.env.PUBLIC_URL + "/images/gorro1.jpg", title: "Gorro Cute", price: 500 },
   { id: 4, image: process.env.PUBLIC_URL + "/images/gorro2.jpg", title: "Gorro", price: 450 },
   { id: 5, image: process.env.PUBLIC_URL + "/images/medias1.jpg", title: "Medias", price: 450 },
-  { id: 6, image: process.env.PUBLIC_URL + "/images/pantalon1.jpg", title: "Pantalon", price: 450 }
+  { id: 6, image: process.env.PUBLIC_URL + "/images/pantalon1.jpg", title: "Pantalón", price: 450 }
 ];
