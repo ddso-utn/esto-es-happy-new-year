60d97f3e62a4397cfbf3ed892a868ec4e512c812
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2025-06-05 19:46:03 -0300

    Adding title

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,9 +1,14 @@
 import Carousel from "../../components/carousel/Carousel";
 import './Home.css'
+import {Button, TextField} from "@mui/material";
 
 const Home = () => {
   return (
     <div className="root">
+      <div className="title">
+        <h1 className="collection-name">Winter 2025</h1>
+        <span>NEW COLLECTION</span>
+      </div>
       <Carousel/>
     </div>
   )
